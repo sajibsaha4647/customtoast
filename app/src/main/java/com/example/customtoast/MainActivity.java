35355632184cefc2bfdeb9e28039448c140db8ca
sajibsaha4647 <sksaha4648@gmail.com>
2021-10-27 00:31:42 +0600

    use custom view

--- app/src/main/java/com/example/customtoast/MainActivity.java
+++ app/src/main/java/com/example/customtoast/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.customtoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button loginbutton;
     private TextView textitem;
+    private TextView texts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         textitem = findViewById(R.id.toasttext);
 
+
+
+
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.getId() == R.id.toasttext){
-                    Toast.makeText(MainActivity.this,"here",Toast.LENGTH_SHORT).show();
-                    textitem.setText("this is my love");
-                }
+//                if(v.getId() == R.id.toasttext){
+//                    Toast.makeText(MainActivity.this,"here",Toast.LENGTH_SHORT).show();
+//                    textitem.setText("this is my love");
+//                }
                 LayoutInflater inflater =  getLayoutInflater() ;
 
              View customView  = inflater.inflate(R.layout.customtoast_layout, (ViewGroup) findViewById(R.id.customToastid));
+
+                texts =  customView.findViewById(R.id.toasttext);
+
+                texts.setText("sajib you are lucky");
+
+//                textitem.setText("this is my love");
 
                 Toast toast = new Toast(MainActivity.this);
                 toast.setDuration(Toast.LENGTH_SHORT);
